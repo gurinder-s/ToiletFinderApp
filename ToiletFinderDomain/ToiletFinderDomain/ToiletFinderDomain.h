47a6a979feeca0c7cdf3029704c217869f9e4c78
Gurinder Singh <gurindersingh@Gurinders-MacBook-Pro-2.local>
2024-02-27 23:52:21 +0000

    TL-1

---
+++ ToiletFinderDomain/ToiletFinderDomain/ToiletFinderDomain.h
@@ -0,0 +1,18 @@
+//
+//  ToiletFinderDomain.h
+//  ToiletFinderDomain
+//
+//  Created by Gurinder Singh on 27/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToiletFinderDomain.
+FOUNDATION_EXPORT double ToiletFinderDomainVersionNumber;
+
+//! Project version string for ToiletFinderDomain.
+FOUNDATION_EXPORT const unsigned char ToiletFinderDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToiletFinderDomain/PublicHeader.h>
+
+
---
+++ ToiletFinderUI/ToiletFinderUI/ToiletFinderUI.h
@@ -0,0 +1,18 @@
+//
+//  ToiletFinderUI.h
+//  ToiletFinderUI
+//
+//  Created by Gurinder Singh on 27/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToiletFinderUI.
+FOUNDATION_EXPORT double ToiletFinderUIVersionNumber;
+
+//! Project version string for ToiletFinderUI.
+FOUNDATION_EXPORT const unsigned char ToiletFinderUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToiletFinderUI/PublicHeader.h>
+
+
