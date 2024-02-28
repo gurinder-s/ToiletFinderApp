887a7de0152fac4363acaa05a2e28b71ab811994
Gurinder Singh <gurindersingh@Gurinders-MacBook-Pro-2.local>
2024-02-28 19:21:20 +0000

    TL-2 Added Firebase

--- ToiletFinder/ToiletFinder/SceneDelegate.swift
+++ ToiletFinder/ToiletFinder/SceneDelegate.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
-
+import Firebase
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        //Mark FIREBASE
+        FirebaseApp.configure()
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
                 window = UIWindow(frame: windowScene.coordinateSpace.bounds)
                 window?.windowScene = windowScene
