a15a66787c52e89142d11423572c9d799ebb71ce
gurindersingh <graphiczz8@gmail.com>
2024-03-16 18:08:37 +0000

    Register Validation and prepping for Authentication checks

---
+++ ToiletFinder/ToiletFinder/Utilities/Validator.swift
@@ -0,0 +1,8 @@
+//
+//  Validator.swift
+//  ToiletFinder
+//
+//  Created by Gurinder Singh on 16/03/2024.
+//
+
+import Foundation
---
+++ ToiletFinderDomain/ToiletFinderDomain/Models/LoginUserRequest.swift
@@ -0,0 +1,8 @@
+//
+//  loginUserRequest.swift
+//  ToiletFinderDomain
+//
+//  Created by Gurinder Singh on 16/03/2024.
+//
+
+import Foundation
